e30ba70213e637ba39aa0f92f946c53b9badd34b
Alexandre D <a.domingues@ecole-ipssi.net>
2022-12-11 12:26:45 +0100

    formation

--- formation.php
+++ formation.php
@@ -3,6 +3,73 @@ $title = "Formation";
 require "head.php";
 ?>
 
+    <div class="main">
+        <div class="text-center mb-5">
+            <h2>La formation</h2>
+        </div>
+        <div class="row mr-5 align-items-center">
+            <div class="col-md-5 text-end">
+                <a href="https://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers"><img
+                            src="doc/LogoBTS-SIO.png" alt=""></a>
+            </div>
+            <div class="col-md-6 text-start text-justify">
+                D’abord le Brevet de Technicien Supérieur Services informatique aux Organisations (<strong>SIO</strong>)
+                s’adresse à ceux qui souhaitent se former en deux ans aux métiers d’administrateur réseau ou de
+                développeur pour intégrer directement le marché du travail ou continuer des études dans le domaine de
+                l’informatique. S'est ajoutée à la nouvelle réforme une partie Cybersécurité pour palier au manque
+                d'enseignement dans ce domaine.
+            </div>
+        </div>
+        <div class="row mt-5 text-justify">
+            <div class="col-md-5 mx-auto">
+                <h4 class="text-center">SLAM</h4>
+                <div class="text-center mt-3 mb-3"><img src="doc/logoSlam.png" width="333" height="121"></div>
+                <p>Le signe SLAM signifie <strong>«&thinsp;Solutions Logicielles et Applications
+                        Métier&thinsp;»</strong>. </p>
+                <p>L’option SLAM est principalement destinée aux étudiants qui s’orientent vers les <strong>métiers
+                        liés à la conception et la maintenance de programmes applicatifs</strong>. Donc grâce à des
+                    cours spécifiques, les diplômés seront capables de gérer un parc informatique ou d’administrer un
+                    réseau au sein d’une entreprise. Aussi ils pourront également gérer l’intégration, la sécurisation
+                    et la configuration des serveurs, mais aussi des postes clients et des équipements d’interconnexion.
+                </p>
+                <p>Voici quelques exemples de métiers que les étudiants peuvent exercer
+                    après le BTS SIO option SLAM :</p>
+                <ul>
+                    <li>Développeur d’applications informatiques</li>
+                    <li>Chargé d’études informatiques</li>
+                    <li>Responsable des services applicatifs</li>
+                    <li>Programmeur analyste</li>
+                    <li>Programmeur d’applications</li>
+                    <li>Technicien d’études informatiques</li>
+                </ul>
+            </div>
+            <div class="col-md-5 mx-auto">
+                <h4 class="text-center">SISR</h4>
+                <div class="text-center mt-3 mb-3"><img src="doc/logoSisr.png" width="333" height="121"></div>
+                <p>L’acronyme SISR signifie <strong>«&thinsp;Solutions d’infrastructure, systèmes et réseaux&thinsp;»</strong>.
+                </p>
+                <p>L’option SISR est donc d’abord destinée aux étudiants qui s’orientent vers les <strong>métiers
+                        liés à la conception et la maintenance d’infrastructures réseaux</strong>. Avant tout cela
+                    correspond à assurer la sécurité, la maintenance et l’installation des réseaux et des équipements
+                    informatiques font partie des principales missions des futurs administrateurs, techniciens ou
+                    pilotes d’exploitation.</p>
+                <p>
+                    Donc voici quelques exemples de métiers que les étudiants peuvent exercer après le BTS SIO option
+                    SISR :
+                </p>
+                <ul>
+                    <li>Technicien réseau — télécoms</li>
+                    <li>Technicien systèmes et réseaux</li>
+                    <li>Administrateur systèmes et réseaux</li>
+                    <li>Support systèmes et réseaux</li>
+                    <li>Technicien d’infrastructure</li>
+                    <li>Pilote d’exploitation</li>
+                    <li>Technicien de production </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 <?php
 require 'footer.html';
 ?>
